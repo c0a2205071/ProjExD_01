170687ead1de722a07f0000cececf86e50c8905e
atsukan <c0a2205071@edu.teu.ac.jp>
2023-04-18 16:10:22 +0900

    commit4

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,10 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [0, 0])
+        if 1600-tmr == 0:
+            tmr = 0 
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
         screen.blit( kk_imgs[tmr%2], [300, 200])
 
         pg.display.update()
